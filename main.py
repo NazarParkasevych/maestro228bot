9601be8b19d72f9752a7478dcdf19636ff013b35
Parkasevych Nazar <parkasevychnazarij@gmail.com>
2024-03-28 15:01:33 +0200

    created telegram bot

--- main.py
+++ main.py
@@ -24,16 +24,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def school(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'You study in VPU29')
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text)
+    message = update.message.text.lower()
+    if 'hello' in message:
+        reply_text = f'Hello {update.effective_user.first_name}!'
+    else:
+        reply_text = 'I dont understand you!'
+    await update.message.reply_text(reply_text)
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("school", school))
-app.add_handler(MessageHandler(filters.TEXT, echo))
-
-
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 
 app.run_polling()
